5bfbc598d66ad70e4ce891eef9736eab99b03c53
Giuseppe Vigneri <geppe1990@gmail.com>
2021-05-12 10:28:30 +0200

    Indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,37 @@
-const webpack = require('webpack');
-const path = require('path');
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const webpack = require("webpack");
+const path = require("path");
 
 const config = {
-  entry: [
-    'react-hot-loader/patch',
-    './src/index.js'
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx'
-    ],
-    alias: {
-      'react-dom': '@hot-loader/react-dom'
-    }
-  },
-  devServer: {
-    contentBase: './dist'
-  }
+	entry: ["react-hot-loader/patch", "./src/index.js"],
+	output: {
+		// eslint-disable-next-line no-undef
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: "babel-loader",
+				exclude: /node_modules/
+			},
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"]
+			}
+		]
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
+		alias: {
+			"react-dom": "@hot-loader/react-dom"
+		}
+	},
+	devServer: {
+		contentBase: "./dist"
+	}
 };
 
 module.exports = config;
