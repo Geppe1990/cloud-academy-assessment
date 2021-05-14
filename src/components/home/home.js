fa4186b7d23e8a89e880a23fd1f3bab17ab5881a
Giuseppe Vigneri <geppe1990@gmail.com>
2021-05-14 12:48:35 +0200

    Homepage + style

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./home.scss";
+import Cta from "../cta/cta";
 
 const Home = () => {
-	return <>Homepage</>;
+	return (
+		<div className="container">
+			<div className="home">
+				<h1>Cloud Academy Assessment</h1>
+				<Cta text={"Go to the first character"} link={"/1"} />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
