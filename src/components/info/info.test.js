c24c201d4be1bd3aae7710482e31a2f53b7ea145
Giuseppe Vigneri <geppe1990@gmail.com>
2021-05-21 16:55:35 +0200

    Snapshot

--- src/components/info/info.test.js
+++ src/components/info/info.test.js
@@ -1,6 +1,34 @@
 import React from "react";
 import Info from "./info";
 
+const character = {
+	id: 13,
+	name: "Alien Googah",
+	status: "unknown",
+	species: "Alien",
+	type: "",
+	gender: "unknown",
+	origin: {
+		name: "unknown",
+		url: ""
+	},
+	location: {
+		name: "Earth (Replacement Dimension)",
+		url: "https://rickandmortyapi.com/api/location/20"
+	},
+	image: "https://rickandmortyapi.com/api/character/avatar/13.jpeg",
+	episode: [
+		"https://rickandmortyapi.com/api/episode/31"
+	],
+	url: "https://rickandmortyapi.com/api/character/13",
+	created: "2017-11-04T20:33:30.779Z"
+}
+
 it("Renders info without crashing", () => {
-	shallow(<Info />);
+	shallow(<Info character={character}/>);
+});
+
+it('match the snapshot', () => {
+	const wrapper = renderer.create(<Info character={character} />).toJSON();
+	expect(wrapper).toMatchSnapshot();
 });
